5281ea238a30113f4cb1b5604584be82ac707720
sahityapavurala <sp3133>
2017-02-25 23:02:00 -0500

    end to end , first cut

--- EmailAnalysis/src/main/resources/run_job.sh
+++ EmailAnalysis/src/main/resources/run_job.sh
@@ -2,6 +2,7 @@ spark-submit \
 --class sp.email.analysis.AnalysisDriver \
 --master yarn \
 --deploy-mode cluster \
---jars spark-csv_2.11-1.5.0.jar \
+--jars spark-csv_2.11-1.5.0.jar,commons-csv-1.1.jar,univocity-parsers-1.5.1.jar \
 target/email-analysis-1.0-SNAPSHOT.jar \
-/user/cloudera/test_input
+/user/cloudera/test/enron_with_categories
+
