9cb782d9cf515b315b6b2f9aaafa1d8e83491042
sahityapavurala <sp3133>
2017-02-28 21:01:02 -0500

    end to end , missing comments

--- EmailAnalysis/src/main/java/sp/email/analysis/transform/OutputTransformer.java
+++ EmailAnalysis/src/main/java/sp/email/analysis/transform/OutputTransformer.java
@@ -7,9 +7,8 @@ import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.hive.HiveContext;
 import org.apache.spark.sql.types.StructType;
-import sp.email.analysis.utils.FilePreprocessor;
 
-import java.util.*;
+
 
 /**
  * Created by sahityapavurala on 2/25/17.
@@ -51,87 +50,18 @@ public class OutputTransformer {
             System.out.println("The top three senders of broadcast emails are ");
             printRows(second);
 
-            String query = "select reply.sender,t1.email_date - t2.email_date as response_time from (select " +
-                    "h1.message_id as id_orig, reply.id_reply from email h1 left join recipient r1 on " +
-                    "h1.message_id = r1.message_id inner join (select h2.message_id as id_reply, " +
-                    "h2.subject as sub_reply, r2.Recipient as recipient_reply, from email h2 left " +
-                    "join recipient r2 on h2.message_id = r2.message_id order by h2.email_date desc ) " +
-                    "reply on reply.recipient_reply = h1.sender and reply.sub_reply = h1.subject and " +
-                    "h1.subject is not null having count(distinct " +
-                    "h1.sender)>1 order by h1.email_date desc) withreply left join email t1 on " +
-                    "withreply.id_orig = t1.message_id left join email t2 on withreply.id_reply = " +
-                    "t2.message_id where t1.sender <> t2.sender and t1.subject is not null order by " +
-                    "response_time";
+            String query = " select t2.message_id,t2.sender,t2.subject,t1.sender,abs(t2.email_date-t1.email_date) as response_time " +
+                    "from email t1 " +
+                    "left outer join email t2 on t1.hash = t2.hash and t1.message_id != t2.message_id and t1.sender != t2.sender " +
+                    "where t2.hash is not null order by response_time";
+
 
             Row[] third = hqlc.sql(query).limit(5).collect();
 
             System.out.println("The nums of rows for third is :: " + third.length);
 
             printRows(third);
-/**
- * SELECT
- start_log.name,
- MAX(start_log.ts) AS start_time,
- end_log.ts AS end_time,
- TIMEDIFF(MAX(start_log.ts), end_log.ts)
- FROM
- log AS start_log
- INNER JOIN
- log AS end_log ON (
- start_log.name = end_log.name
- AND
- end_log.ts > start_log.ts)
- WHERE start_log.eventtype = 'start'
- AND end_log.eventtype = 'stop'
- GROUP BY start_log.name
 
- select res.sender,res.response_time
- from(
- select t2.message_id,t2.hash,t2.sender,t2.subject,
- case when (t2.email_date > t1.email_date ) then (t2.email_date-t1.email_date)
- else end
- )as response_time
- from email t1
- inner join email t2 on t1.hash = t2.hash and t1.message_id != t2.message_id
- inner join recipient t3 on t2.sender = t3.recipient
-  order by response_time
- ) res
-
-
- */
-
-
-
-            HashMap<String, Long> response = new HashMap<String, Long>();
-            String preHash = null;
-            Long pretimeStamp = null;
-
-            for (Row row : third) {
-                String hash = (String) row.get(0);
-                String sender = (String) row.get(1);
-                String message_id = (String) row.get(2);
-                Long email_date = (Long) row.get(3);
-
-                if (hash != preHash) {
-                    preHash = hash;
-                    pretimeStamp = email_date;
-                    continue;
-                } else {
-                    preHash = hash;
-                            //+ "," + subject + "," + sender + "," + recipient;
-                    response.put(message_id, email_date - pretimeStamp);
-                    pretimeStamp = email_date;
-                }
-
-            }
-
-            System.out.println("The size of map is :: " + response.size());
-
-            Map<String, Long> sortedMap = sortByValue(response);
-
-            System.out.println("The size of sorted map is :: " + sortedMap.size());
-
-            printMap(sortedMap, 5);
         }
         catch (Exception e) {
             LOGGER.info("Exception in transform :: " + e.toString());
@@ -153,45 +83,6 @@ public class OutputTransformer {
 
     }
 
-    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> unsortMap) {
-
-        List<Map.Entry<K, V>> list =
-                new LinkedList<Map.Entry<K, V>>(unsortMap.entrySet());
-
-        Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
-            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
-                return (o1.getValue()).compareTo(o2.getValue());
-            }
-        });
-
-        Map<K, V> result = new LinkedHashMap<K, V>();
-        for (Map.Entry<K, V> entry : list) {
-            result.put(entry.getKey(), entry.getValue());
-        }
-
-        return result;
-
-    }
-
-    public static <K, V> void printMap(Map<K, V> map,int num) {
-        int count = 0;
-        System.out.println("The Fastest response times are ::");
-        try {
-            for (Map.Entry<K, V> entry : map.entrySet()) {
-                System.out.println("Printing inside sorted map");
-                if (count < num) {
-                    System.out.println("Key : " + entry.getKey() + " Value : " + entry.getValue());
-                    count += 1;
-                } else
-                    break;
-            }
-        }
-        catch (Exception e){
-            System.out.println("Exception in printing sorted map :: " + e.toString());
-            e.printStackTrace();
-        }
-
-    }
 
     public static void printRows(Row[] rows){
 
