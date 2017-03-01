721c7beb2607dcbf35250436d0ecdfbd98c9039a
sahityapavurala <sp3133>
2017-02-28 23:17:16 -0500

    end to end , complete

--- EmailAnalysis/src/main/java/sp/email/analysis/utils/Constants.java
+++ EmailAnalysis/src/main/java/sp/email/analysis/utils/Constants.java
@@ -15,8 +15,9 @@ public class Constants {
             "where e.label = 'direct' " +
             "group by recipient order by cnt desc";
 
-    public static final String CREATE_RESPONSE_TIMES = "select sender,count(*) as cnt from email where label='broadcast' " +
-            "group by sender order by cnt desc";
+    public static final String CREATE_RESPONSE_TIMES = "select t2.message_id,t2.sender,t2.subject,t1.sender,abs(t2.email_date-t1.email_date) as response_time " +
+            "from email t1 left outer join email t2 on t1.hash = t2.hash and t1.message_id != t2.message_id and t1.sender != t2.sender " +
+            "where t2.hash is not null order by response_time";
 
     public static final String DELIMITER = ",";
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormat.forPattern("EEE, dd MMM yyyy HH:mm:ss Z (z)").withZoneUTC();
--- EmailAnalysis/src/main/resources/run_job.sh
+++ EmailAnalysis/src/main/resources/run_job.sh
@@ -4,5 +4,4 @@ spark-submit \
 --deploy-mode cluster \
 --jars spark-csv_2.11-1.5.0.jar,commons-csv-1.1.jar,univocity-parsers-1.5.1.jar \
 target/email-analysis-1.0-SNAPSHOT.jar \
-/user/cloudera/test/enron_with_categories
-
+{1}
