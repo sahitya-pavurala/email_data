c4aa1cf4a8113fe9b1c676183d2f3d7875da63c5
sahityapavurala <sp3133>
2017-02-28 23:39:57 -0500

    end to end complete

--- EmailAnalysis/src/main/java/sp/email/analysis/utils/Constants.java
+++ EmailAnalysis/src/main/java/sp/email/analysis/utils/Constants.java
@@ -17,7 +17,7 @@ public class Constants {
 
     public static final String CREATE_RESPONSE_TIMES = "select t2.message_id,t2.sender,t2.subject,t1.sender,abs(t2.email_date-t1.email_date) as response_time " +
             "from email t1 left outer join email t2 on t1.hash = t2.hash and t1.message_id != t2.message_id and t1.sender != t2.sender " +
-            "where t2.hash is not null order by response_time";
+            "where t1.hash is not null and t2.hash is not null order by response_time";
 
     public static final String DELIMITER = ",";
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormat.forPattern("EEE, dd MMM yyyy HH:mm:ss Z (z)").withZoneUTC();
